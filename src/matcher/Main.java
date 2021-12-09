4822b741cad6294557a509fa7a5fd6e71114ede7
Muhammad S Shamim <sa501428@gmail.com>
2021-12-09 05:12:30 -0600

    update usage

--- src/matcher/Main.java
+++ src/matcher/Main.java
@@ -9,6 +9,12 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Incorrect number of arguments");
+            System.err.println("Usage: chrMatcher.jar <experiment_chrM.map.txt>");
+            System.exit(7);
+        }
+
         List<Individual> individuals = (new IndividualVault()).load();
         MysteryLibrary mysteryLibrary = new MysteryLibrary(args[args.length - 1]);
 
